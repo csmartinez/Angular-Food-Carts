8b9e452d81099e09db8ee5a632fa035f47a03ece
Carli Martinez and Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-23 11:12:09 -0800

    add CRUD functionality to foodcart list

---
+++ app.js
@@ -0,0 +1,1 @@
+var foodcartRoster = angular.module('foodcartRoster', []);
---
+++ js/foodcartsController.js
@@ -0,0 +1,20 @@
+foodcartRoster.controller('FoodCartsCtrl', function FoodCartsCtrl($scope) {
+  $scope.foodcarts = [];
+  $scope.addCart = function() {
+    $scope.foodcarts.push({
+      name: $scope.foodCartName,
+      location: $scope.foodCartLocation,
+      type: $scope.foodCartType,
+      pricerange: $scope.foodCartPrice
+    });
+    $scope.foodCartName = null;
+    $scope.foodCartLocation = null;
+    $scope.foodCartType = null;
+    $scope.foodCartPrice = null;
+  };
+  $scope.deleteFoodCart = function(cart) {
+    var index = $scope.foodcarts.indexOf(cart);
+    $scope.foodcarts.splice(index,1);
+
+  };
+})
